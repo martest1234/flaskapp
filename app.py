443de5006f074e78e0baa8e713eb7434f0e0c160
martest1234 <martest1234@wp.pl>
2026-01-08 20:31:05 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+@app.route("/test")
+def test():
+    return "<h1>simple flask test</h1>"
